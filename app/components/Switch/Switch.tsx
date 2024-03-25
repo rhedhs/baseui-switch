1dba47e28648be93af60eeb4ab008f3bf1c65fe4
ELFANNIR ABDERRAHMANE <aelfannir@gmail.com>
2024-03-25 11:16:02 +0000

    Improve readability

--- app/components/Switch/Switch.tsx
+++ app/components/Switch/Switch.tsx
@@ -21,30 +21,26 @@ const Switch = forwardRef<HTMLSpanElement, SwitchProps>((props, ref) => {
                     return {
                         ...resolvedSlotProps,
                         className: clsx(
-                            `group relative inline-block w-12 h-6 m-2.5  ${
-                                ownerState.disabled
-                                    ? "cursor-not-allowed opacity-40"
-                                    : "cursor-pointer"
-                            }`,
+                            "group relative inline-block w-12 h-6 m-2.5",
+                            ownerState.disabled ? "cursor-not-allowed opacity-40" : "cursor-pointer",
                             resolvedSlotProps?.className
                         ),
                     };
                 },
-                // Here we are overriding the input, track and thumb slots
                 input: (ownerState) => {
                     const resolvedSlotProps = resolveSlotProps(
                         props.slotProps?.input,
                         ownerState
                     );
+
                     return {
                         ...resolvedSlotProps,
                         className: clsx(
-                            "cursor-pointer absolute w-full h-full top-0 left-0 opacity-0 z-10 border-none ",
+                            "cursor-pointer absolute w-full h-full top-0 left-0 opacity-0 z-10 border-none",
                             resolvedSlotProps?.className
                         ),
                     };
                 },
-                // Customizing the track
                 track: (ownerState) => {
                     const resolvedSlotProps = resolveSlotProps(
                         props.slotProps?.track,
@@ -54,31 +50,23 @@ const Switch = forwardRef<HTMLSpanElement, SwitchProps>((props, ref) => {
                     return {
                         ...resolvedSlotProps,
                         className: clsx(
-                            `absolute block w-full h-full transition rounded-full  outline-none border-slate-300 dark:border-gray-700 group-[.base--focusVisible]:shadow-outline-switch border border-yellow-600 dark:border-slate-600
-                            
-                ${
-                                ownerState.checked
-                                    ? "bg-slate-900"
-                                    : "bg-slate-100 dark:bg-slate-900 hover:bg-slate-200 dark:hover:bg-slate-800"
-                            } `,
+                            "absolute block w-full h-full transition rounded-full  outline-none border-slate-300 dark:border-gray-700 group-[.base--focusVisible]:shadow-outline-switch border border-yellow-600 dark:border-slate-600",
+                            ownerState.checked ? "bg-slate-900": "bg-slate-100 dark:bg-slate-900 hover:bg-slate-200 dark:hover:bg-slate-800",
                             resolvedSlotProps?.className
                         ),
                     };
                 },
-                // Customizing the thumb
                 thumb: (ownerState) => {
                     const resolvedSlotProps = resolveSlotProps(
                         props.slotProps?.thumb,
                         ownerState
                     );
+
                     return {
                         ...resolvedSlotProps,
                         className: clsx(
-                            `block w-4 h-4 top-1 rounded-2xl   outline-none border-slate-300 dark:border-gray-700 transition ] ${
-                                ownerState.checked
-                                    ? "left-[26px] bg-[url('/svg/moon.svg')] "
-                                    : "left-[4px] bg-[url('/svg/sun.svg')]"
-                            }  relative transition-all duration-200 ease-linear`,
+                            "block w-4 h-4 top-1 rounded-2xl   outline-none border-slate-300 dark:border-gray-700 transition ] relative transition-all duration-200 ease-linear",
+                            ownerState.checked ? "left-[26px] bg-[url('/svg/moon.svg')]": "left-[4px] bg-[url('/svg/sun.svg')]",
                             resolvedSlotProps?.className
                         ),
                     };
